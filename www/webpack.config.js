4a2433489348928ff612412b115a051247f729f5
Sigurd Tullander <legotull.99@gmail.com>
2023-05-15 15:42:46 +0200

    Use workbox-webpack-plugin to generate serviceworker

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -1,21 +1,26 @@
+const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const {GenerateSW} = require("workbox-webpack-plugin");
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: "./index_loader.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "index_loader.js",
   },
   mode: "development",
   plugins: [
     new CopyWebpackPlugin([
-      'index.html',
-      'register_serviceworker.js',
-      'serviceworker.js',
-      'manifest.json',
-      'icon.ico',
-      'icon.png',
-    ])
+      "index.html",
+      "register_serviceworker.js",
+      "manifest.json",
+      "icon.ico",
+      "icon.png",
+      "node_modules/bootstrap/dist/css/bootstrap.min.css",
+      "node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+    ]),
+    new GenerateSW({
+      swDest: "serviceworker.js",
+    }),
   ],
 };
